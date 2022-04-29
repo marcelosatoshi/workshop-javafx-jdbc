e80843367b8764a2db1a9c4b0399de68e113a097
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-04-29 04:00:05 -0300

    Passando um objeto Department para o formulário

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,8 +9,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
+
+	private Department entity;
 
 	@FXML
 	private TextField txtId;
@@ -36,6 +39,10 @@ public class DepartmentFormController implements Initializable {
 	public void onBtCancelAction() {
 		System.out.println("ta peganndo");
 	}
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -48,4 +55,12 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldMaxLength(txtName, 20);
 	}
 
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
 }
